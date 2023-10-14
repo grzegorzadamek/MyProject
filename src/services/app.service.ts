ffb8e6597b846d33a91a2bf766d2cc4d62139771
grzegorzadamek <grzegorzadamek@outlook.com>
2023-10-14 17:04:21 +0200

    small fix

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -15,7 +15,7 @@ export class AppService {
   }
 
   public getMeetingByPost(data: string): Observable<any> {
-     return this.http.post<any>('http://grzegorzadamek.ct8.pl:6321/data', {data}
+     return this._http.post<any>('http://grzegorzadamek.ct8.pl:6321/data', {data}
 //      return this.http.post<any>('http://localhost:3000/data', {data}
      );
   }
